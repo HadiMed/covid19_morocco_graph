8928c9c94d347042085f3cbd3f5a1a26644ec9ac
Mohammed Hadi <57273771+HadiMed@users.noreply.github.com>
2021-09-06 13:23:26 +0100

    Update morocco_cases.py

--- morocco_cases.py
+++ morocco_cases.py
@@ -1,14 +1,18 @@
 import PyPDF2 
 import re 
-from datetime import date  
+from datetime import datetime , timedelta , date
+import random  
 import requests 
 import matplotlib.pyplot as plt 
+from bokeh.io import curdoc 
+from bokeh.plotting import figure , show , output_file
+from bokeh.models import DatetimeTickFormatter
 
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
 #working from range ( specific pase date to today ) 
-start_date = "01.7.21"
+start_date = "01.3.21"
 
 
 today_date = date.today()  
@@ -27,7 +31,7 @@ start_day , start_month ,year =  [int(x) for x in start_date.split('.') ]
 
 end_dat , end_month , teeee = [int(x) for x in date_end.split('.') ]
 
-dates = []
+dates = [start_date] 
 while start_day != end_dat or start_month !=end_month :
 	if start_day != 31 :
 		start_day+=1
@@ -85,19 +89,40 @@ for date in dates :
 				if x[0] <= '9' and x[0] >= '0' :
 					data.append(x) 
 		#print(data)
-
-		new_cases.append(int(data[2]))
+		if (int(data[2]) < 100) : 
+			new_cases.append(new_cases[-1])
+		else :
+			new_cases.append(int(data[2]))
 
 		print(date+" new_cases : "+str(new_cases[-1]))
 	
 	else :
 		print("no response ")	
+		new_cases.append(new_cases[-1])
 
+# using matplotlib
+"""
 dates = [x.replace('.','/')[:5] for x in dates ]
 plt.plot(dates , new_cases) 
 
 plt.xlabel("x - days")
 plt.xlabel("y - new_cases")
 
-plt.show()
+plt.show() """
+# using bokeh 
 
+output_file("trash.html")
+ 
+curdoc().theme = "caliber"
+
+dates = [(datetime.now() - timedelta(day)) for day in range(0, 180) ][::-1]
+
+p = figure( title="Covid cases morocco",x_axis_type="datetime",sizing_mode = "stretch_width", x_axis_label="dates", y_axis_label="cases", plot_height=700,
+    max_width=1600) 
+
+p.line(dates , new_cases , color="navy", line_width=1) 
+p.circle(dates, new_cases, size=8)
+p.xaxis[0].formatter = DatetimeTickFormatter(months="%b %Y")
+print(dates)
+print(new_cases) 
+show(p)
